e672627db0c5b559179f7e31291991c446bab8fc
steigal <maxi.steidl@t-online.de>
2013-05-27 15:05:04 +0200

    developing pw_counter
    If you make more than 5 invalid logins you can't login any more.

--- Site/utils/login.php
+++ Site/utils/login.php
@@ -5,30 +5,36 @@ $password = mysql_real_escape_string($_REQUEST['password']);
 
 $result = mysql_query("SELECT * FROM user WHERE email = '".$email."'");
 $row = mysql_fetch_array($result);
-if ($row['password'] == $password)
+if($row['counter'] < 5)
 {
+     if ($row['password'] == $password)
+     {
+             $query = "UPDATE user SET status = 1, counter = 0 WHERE email = '".$email."'";
 
-         $query = "UPDATE user SET status = 1, counter = 0 WHERE email = '".$email."'";
+             $update_status = mysql_query($query);
 
-         $update_status = mysql_query($query);
+             $_SESSION['login'] = 1;
 
-         $_SESSION['login'] = 1;
+             $_SESSION['user_ID'] = $row['user_ID'];
 
-         $_SESSION['user_ID'] = $row['user_ID'];
+             include('../content/user_interface.php');
+     }
+     else
+     {
+              echo "Login invalid for ".$email;
 
-         include('../content/user_interface.php');
+              $pw_counter = $row['counter'];
+              $pw_counter = $pw_counter + 1;
+
+              $query = "UPDATE user SET counter = ".$pw_counter." WHERE email = '".$email."'";
+              $update_status = mysql_query($query);
+
+              echo "<br>This is your ".$pw_counter.". invalid login!";
+     }
 }
 else
 {
-         echo "Login invalid for ".$email;
-
-         $pw_counter = $row['counter'];
-         $pw_counter = $pw_counter + 1;
-
-         $query = "UPDATE user SET counter = ".$pw_counter." WHERE email = '".$email."'";
-         $update_status = mysql_query($query);
-
-         echo "<br>Dies ist Ihr ".$pw_counter.". Fehlversuch!";
+        echo "Your account '".$email."' was disabled. Check your emails!";
 }
 
 mysql_close($link);
