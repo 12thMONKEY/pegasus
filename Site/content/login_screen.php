77de3c21dade8ee80375e15213169f11923bfd85
12thMONKEY <email.andre@gmx.net>
2013-05-28 15:05:35 +0200

    Some uncommited changes i forgot :(

--- Site/content/login_screen.php
+++ Site/content/login_screen.php
@@ -1,11 +1,18 @@
 <div class="fullscreen">
-	<div class="flipWrapper">
+	<div class="loginWrapper">
+		<h2 class="loginHeader">Login</h2>
 		<form class="login" id="login" action="utils/login.php">
-			<input type="text" placeholder="E-Mail" id="email" />
-			<input type="password" placeholder="Passwort" id="password" />
-			<input type="submit" class="loginButton" value="Login" />
-			<a href="#" class="registerLink">Register here</a>
+			<div class="formDivider top">
+				<input type="text" placeholder="E-Mail" id="email" />
+				<input type="password" placeholder="Passwort" id="password" />
+			</div>
+			<div class="formDivider bottom">
+				<button type="submit" class="loginButton">Login</button>
+				<a href="#" class="registerLink">Register here</a>
+			</div>
 		</form>
+	</div>
+	<div class="registerWrapper">
 		<form class="register" id="register" action="utils/register.php">
 			<input type="text" placeholder="E-Mail" id="reg_email" />
 			<input type="password" placeholder="Passwort" id="reg_password" />
@@ -13,8 +20,5 @@
 			<a href="#" class="backLink">Back</a>
 		</form>
 		<div class="loginBack"></div>
-	</div>
-	<div class="hole">
-
 	</div>
 </div>
