1d11d52395f7f8cc2aafb1ea184327b2a9490723
steigal <maxi.steidl@t-online.de>
2013-06-10 19:54:13 +0200

    developing webview
    Works! But still some great design failures!

--- App/src/app/pegasus/MainActivity.java
+++ App/src/app/pegasus/MainActivity.java
@@ -1,15 +1,31 @@
 package app.pegasus;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
+	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		setContentView(R.layout.activity_main);
+		
+		WebView myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.loadUrl("http://192.168.2.111/07_Internet/GitHub/pegasus/Site/index.php");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 	}
 
 	@Override
