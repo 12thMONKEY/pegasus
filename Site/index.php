1089eb6fb856ca655e71c6cb4daf7d4edcd4826d
steigal <maxi.steidl@t-online.de>
2013-06-12 17:53:38 +0200

    fixed conflict
    just ignore..

--- Site/index.php
+++ Site/index.php
@@ -2,38 +2,38 @@
 require_once ('utils/connect_db.php');
 
 if (!isset($_SESSION['date'])) {
-	$timestamp = time();
-	$_SESSION['date'] = date('H:i:s', $timestamp);
+        $timestamp = time();
+        $_SESSION['date'] = date('H:i:s', $timestamp);
 }
 
 if (!isset($_SESSION['login'])) {
-	$_SESSION['login'] = 0;
+        $_SESSION['login'] = 0;
 }
 ?>
 
 <!doctype html>
 
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Pegasus</title>
-		<link rel="stylesheet" href="design/design.css" type="text/css">
-		<script src="http://code.jquery.com/jquery-latest.pack.js" type="text/javascript"></script>
-		<script src="js/md5.js" type="text/javascript"></script>
-		<script src="js/main.js" type="text/javascript"></script>
-	</head>
-	<body>
-		<div class="PGS_wrapper">
-			<?php
-	
-			if ($_SESSION['login'] == 1) {
-				include ('content/user_interface.php');
-			} else {
-				include ('content/login_screen.php');
-			}
-			?>
-		</div>
-	</body>
+        <head>
+                <meta charset="UTF-8">
+                <title>Pegasus</title>
+                <link rel="stylesheet" href="design/design.css" type="text/css">
+                <script src="http://code.jquery.com/jquery-latest.pack.js" type="text/javascript"></script>
+                <script src="js/md5.js" type="text/javascript"></script>
+                <script src="js/main.js" type="text/javascript"></script>
+        </head>
+        <body>
+                <div class="PGS_wrapper">
+                        <?php
+
+                        if ($_SESSION['login'] == 1) {
+                                include ('content/user_interface.php');
+                        } else {
+                                include ('content/login_screen.php');
+                        }
+                        ?>
+                </div>
+        </body>
 </html>
 <?php
 mysql_close($link);
