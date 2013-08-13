b29d0ae9718a869267db8767480fc09470cdc28e
steigal <maxi.steidl@t-online.de>
2013-08-13 22:53:19 +0200

    new dump

--- Site/dumps/pegasus.sql
+++ Site/dumps/pegasus.sql
@@ -1,9 +1,9 @@
-﻿-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 3.5.2.2
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 16. Jul 2013 um 20:59
+-- Erstellungszeit: 13. Aug 2013 um 22:53
 -- Server Version: 5.5.27
 -- PHP-Version: 5.4.7
 
@@ -56,8 +56,6 @@ CREATE TABLE IF NOT EXISTS `events` (
   `subject` varchar(256) COLLATE utf8_bin NOT NULL,
   `describtion` text COLLATE utf8_bin NOT NULL,
   `owner_ID` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `end_date` datetime NOT NULL,
   `location` text COLLATE utf8_bin NOT NULL,
   `start_year` int(4) NOT NULL,
   `start_month` int(2) NOT NULL,
@@ -67,51 +65,30 @@ CREATE TABLE IF NOT EXISTS `events` (
   `end_month` int(2) NOT NULL,
   `end_day` int(2) NOT NULL,
   `end_time` time NOT NULL,
-
-
   PRIMARY KEY (`event_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=19 ;
 
 --
 -- Daten für Tabelle `events`
 --
 
-INSERT INTO `events` (`event_ID`, `subject`, `describtion`, `owner_ID`, `date`, `end_date`, `location`) VALUES
-(1, 'Sex', 0x486965722077657264656e20776972205365782c2050617274792c20506f6d6d65732066656965726e2121, 1, '2013-06-26 07:21:23', '2013-06-27 03:03:00', 0x4d756e69632045617374203a44),
-(2, 'Abiturfeier!!', 0x48696572207769726420616269696969696969696974757220676566656965727421, 1, '2013-06-28 04:23:44', '2013-06-29 15:00:00', 0x59686969696861616161203a44205765206c6f76652043616b6573);
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `messages`
---
-
-CREATE TABLE IF NOT EXISTS `messages` (
-  `message_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `content` text COLLATE utf8_unicode_ci NOT NULL,
-  `send_date` datetime NOT NULL,
-  `user_ID` int(11) NOT NULL,
-  `message_status` int(11) NOT NULL,
-  `conversation_ID` int(11) NOT NULL,
-  PRIMARY KEY (`message_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=12 ;
-
---
--- Daten für Tabelle `messages`
---
-
-INSERT INTO `messages` (`message_ID`, `content`, `send_date`, `user_ID`, `message_status`, `conversation_ID`) VALUES
-(1, 'ljdfÃ¤lsdkfnldsfnblds', '2013-07-08 03:21:08', 2, 0, 1),
-(2, 'Message 2', '2013-07-08 04:17:15', 2, 0, 2),
-(3, 'Message 3', '2013-07-08 04:17:26', 2, 0, 3),
-(4, 'Message 4', '2013-07-08 04:17:32', 2, 0, 4),
-(5, 'Message 5', '2013-07-08 04:17:38', 2, 0, 5),
-(6, 'hallo123', '2013-07-08 04:48:24', 2, 0, 0),
-(7, 'hallo123', '2013-07-08 04:49:15', 2, 0, 0),
-(8, 'ydhdyhaedgr', '2013-07-08 04:49:40', 2, 0, 0),
-(9, 's&lt;acascas', '2013-07-08 04:52:23', 2, 0, 0),
-(10, 'dbgsdb db ebvedved ', '2013-07-08 04:55:42', 2, 0, 6),
-(11, 'dfgdfgdfg', '2013-07-08 05:03:48', 2, 0, 7);
+INSERT INTO `events` (`event_ID`, `subject`, `describtion`, `owner_ID`, `location`, `start_year`, `start_month`, `start_day`, `start_time`, `end_year`, `end_month`, `end_day`, `end_time`) VALUES
+(3, 'Pegasus Projekt', 0x486965722067656874732072756e6420756e642064617320506567617375732050726f6a656b74207769726420766f727467657365747a742121, 1, 0x4d535720626961746368, 2013, 7, 7, '17:00:00', 2013, 7, 7, '18:00:00'),
+(4, 'Alter Eintrag', 0x617367, 1, 0x61736466, 2013, 6, 8, '20:55:22', 2013, 6, 9, '21:55:00'),
+(5, 'Pegasus Projekt 2.0', 0x4a65747a206765687420646965205061727479206c6f7321, 1, 0x48696572206d61616e, 2013, 7, 7, '19:00:00', 2013, 7, 7, '20:00:00'),
+(6, 'Pegasus Projekt 4.0', 0x4869207468657265, 1, 0x6869, 2013, 7, 10, '12:00:00', 2013, 7, 10, '14:00:00'),
+(7, 'Pegasus Projekt 3.0', 0xc3b66f6861757366, 1, 0x736c6662, 2013, 7, 9, '12:00:00', 2013, 7, 9, '14:00:00'),
+(8, 'Neue Musik', 0x48696572, 1, 0x48696572, 2013, 7, 17, '01:16:00', 2013, 7, 17, '05:00:00'),
+(9, 'Asch', 0x48696572, 1, 0x48696572, 2013, 7, 17, '01:16:00', 2013, 7, 17, '05:00:00'),
+(10, 'Asch', 0x48696572, 1, 0x48696572, 2013, 7, 17, '01:16:00', 2013, 7, 17, '05:00:00'),
+(11, 'Seefahren', 0x5769722066616872656e207a757220536565, 1, 0x416d20536565, 0, 0, 0, '00:00:00', 0, 0, 0, '00:00:00'),
+(12, 'Seefahren', 0x5769722066616872656e207a757220536565, 1, 0x416d20536565, 2013, 7, 1, '00:00:00', 2013, 7, 1, '00:00:00'),
+(13, 'Seefahren', 0x5769722066616872656e207a757220536565, 1, 0x416d20536565206f69646121, 2013, 7, 1, '00:00:00', 2013, 7, 1, '00:00:00'),
+(14, 'Skifahren', 0x48657574652066616872656e2077697220736b69, 1, 0x496d20416c70656e6c616e6465, 2013, 7, 12, '00:00:00', 2013, 7, 12, '00:00:00'),
+(15, 'Hallali', 0x496368206d6167204bc383c2a473657361686e65746f727465, 1, 0x4869, 2013, 7, 11, '00:00:00', 2013, 7, 11, '00:00:00'),
+(16, '', '', 1, '', 2013, 7, 2, '04:39:00', 2013, 7, 2, '09:33:00'),
+(17, 'Geburtstag', 0x486575746520686173742064752047656275727473746167, 1, 0x496e205374757474676172743f3f, 2013, 2, 15, '00:00:00', 2013, 2, 15, '11:59:00'),
+(18, 'yxybyb', 0x7963627962, 2, 0x797862787962, 2013, 7, 2, '01:00:00', 2013, 7, 2, '01:00:00');
 
 -- --------------------------------------------------------
 
@@ -173,8 +150,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`user_ID`, `last_name`, `first_name`, `title`, `email`, `password`, `birthday`, `status`, `counter`, `reg_date`, `last_login`) VALUES
-(1, 'Steidl', 'Maxi', 1, 'maxi.steidl@t-online.de', '10b43971a8295f3720f38fbcdd9d6ac6', '1994-02-15', 1, 0, '2013-06-05', '2013-07-07 20:35:49'),
-(2, 'Schmidt', 'André', 1, 'email.andre@gmx.net', 'ecfcb09d14771e375f4a7ed2aa6b25c5', '1994-05-23', 1, 0, '2013-06-05', '2013-07-07 22:02:12'),
+(1, 'Steidl', 'Maxi', 1, 'maxi.steidl@t-online.de', '10b43971a8295f3720f38fbcdd9d6ac6', '1994-02-15', 0, 0, '2013-06-05', '2013-08-13 22:31:42'),
+(2, 'Schmidt', 'André', 1, 'email.andre@gmx.net', 'ecfcb09d14771e375f4a7ed2aa6b25c5', '1994-05-23', 1, 0, '2013-06-05', '2013-08-13 22:47:22'),
 (4, 'Mustermann', 'Max', 1, 'max.mustermann@muster.web', '3a81de5f99818842e71bbcb2c420ae49', '2000-02-19', 1, 0, '2013-06-05', '2013-06-14 02:53:05');
 
 -- --------------------------------------------------------
@@ -216,8 +193,17 @@ CREATE TABLE IF NOT EXISTS `user_events_lookup` (
 --
 
 INSERT INTO `user_events_lookup` (`user_ID`, `event_ID`, `attendance_state`) VALUES
-(1, 1, 0),
-(1, 2, 0);
+(1, 3, 0),
+(1, 4, 0),
+(1, 5, 0),
+(1, 6, 0),
+(1, 7, 0),
+(1, 13, 0),
+(1, 14, 0),
+(1, 15, 0),
+(1, 16, 0),
+(1, 17, 0),
+(2, 18, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
